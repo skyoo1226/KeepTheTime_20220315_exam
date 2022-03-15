f1368ca9f3e1ae02daace353b04b6ec3dec3f543
skyoo1226 <you1226@naver.com>
2022-03-15 21:14:18 +0900

    Initial commit

---
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.skyoo.keepthetime_20220315_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.skyoo.keepthetime_20220315_exam.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
