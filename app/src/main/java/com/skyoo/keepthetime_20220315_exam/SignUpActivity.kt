106bc8279060b0581b2bc552c95a38e6e5efbb9b
skyoo1226 <you1226@naver.com>
2022-03-16 01:04:42 +0900

    회원가입화면을 BaseActivity 상송으로 변경 함.

--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/SignUpActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/SignUpActivity.kt
@@ -5,12 +5,27 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.skyoo.keepthetime_20220315_exam.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+
+// BaseActivity 만든 후 앞으로는 서버 AppCompatActivity()에서 직접 상속 받지 않고 이제는 BaseActivity에서 받을것임.
+// 그러므로 BaseActivity()를 상속 받을려면, 반드시 setupEvens()와 setValues()를 써야 함. (SignUpActivity에 에러 발생)
+// (SignUpActivity에 에러 발생) 에러 해결법 : SignUpActivity 에서 Alt+enter로 implement members 2개다 추가시킴.
+// 그러면 아래 setupEvens()와 setValues() 코딩 필요 없이 자동 생성.
+class SignUpActivity : BaseActivity() {
 
     lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView( this, R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+        setupEvens()
+        setValues()
+    }
+
+    override fun setupEvens() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
