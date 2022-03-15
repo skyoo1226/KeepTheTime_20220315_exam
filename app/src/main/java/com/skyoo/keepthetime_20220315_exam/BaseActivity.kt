44611c10946989356864d118e97884db7db95246
skyoo1226 <you1226@naver.com>
2022-03-15 21:37:26 +0900

    API를 위한 그래들 등 초기세팅중 - gradle : 3개 추가 retrofit 등등 - activity_main : layout 추가 - manifests : user...INTERNET 추가 - MainActivity 3종류  : lateinit var binding : ActivityMainBinding  : binding = DataBindingUtil.setContentView(this, R.layout.activity_main)  : setupEvents와 setValues 추가

---
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/BaseActivity.kt
@@ -0,0 +1,8 @@
+package com.skyoo.keepthetime_20220315_exam
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    
+}
--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/MainActivity.kt
@@ -2,6 +2,7 @@ package com.skyoo.keepthetime_20220315_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.skyoo.keepthetime_20220315_exam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setupEvents()
         setValues()
     }
