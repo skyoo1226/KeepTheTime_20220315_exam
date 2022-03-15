13c65e5e318e5964837153e78d1cd08f05160dad
skyoo1226 <you1226@naver.com>
2022-03-16 00:26:02 +0900

    회원가입 화면의 데이터바인딩 세팅.  - 새로 SignUpActivity 액티비티 추가후  - lateinit var binding: ActivitySignUpBinding => 추가  - binding = DataBindingUtil.setContentView( this, R.layout.activity_sign_up) => 수정

--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/SignUpActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/SignUpActivity.kt
@@ -2,10 +2,16 @@ package com.skyoo.keepthetime_20220315_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.skyoo.keepthetime_20220315_exam.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView( this, R.layout.activity_sign_up)
     }
+
 }
