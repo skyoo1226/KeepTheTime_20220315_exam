4e539028cdd0865bd442becb569310b4d96f7c0e
skyoo1226 <you1226@naver.com>
2022-03-16 21:30:45 +0900

    메인화면 구조 변경 중 :xml만 우선 - 탭레이아웃 - 뷰페이저 - 2개의 프래그먼트 생성(약속목록, 내정보)

--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/MainActivity.kt
@@ -26,23 +26,20 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+/**
+//        임시 연습문제용 3월13일 3:47:39 내용
 //        서버에서 유저정보(토큰값 포함)을 가져오고, 기기에 저장한 것을 ContextUtil을 통해 가져옴
         apiList.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object :Callback<BasicResponse> {
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
-
                 if (response.isSuccessful) {
                     val br = response.body()!!
-
                     binding.txtMyNickname.text = br.data.user.nick_name
                 }
-
             }
-
             override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
-
             }
-
         })
-
+ **/
     }
+
 }
