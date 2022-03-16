824366df6065a2a13c76e4079c665ee6ed377c9f
skyoo1226 <you1226@naver.com>
2022-03-17 00:36:34 +0900

    자동로그인 기능 구현 중 -실제 내정보 조회에 성공시에만, 메인화면으로 이동하는 로직

--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/SplashActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/SplashActivity.kt
@@ -1,7 +1,15 @@
 package com.skyoo.keepthetime_20220315_exam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.skyoo.keepthetime_20220315_exam.datas.BasicResponse
+import com.skyoo.keepthetime_20220315_exam.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +24,34 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        var isMyINfoOk = false
+        apiList.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+                    isMyINfoOk = true
+                }
+            }
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed( {
+            val isAutoLogin = ContextUtil.getAutoLogin(mContext)
+//            val isMyINfoOk = true  => 이것보다 더 이전에 맞는지 조회 : var로 만듬.
+
+            if (isAutoLogin && isMyINfoOk) {
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)
+            }
+            else {
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                startActivity(myIntent)
+            }
+            finish()
+        }, 2500)
 
     }
 }
