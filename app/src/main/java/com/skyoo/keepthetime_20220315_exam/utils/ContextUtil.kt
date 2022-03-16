17cb4e962380a3f3ba422b0cdea4a32bda4ae557
skyoo1226 <you1226@naver.com>
2022-03-16 20:37:33 +0900

    토큰값을 조회하고 저장하는 기능 추가

--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/utils/ContextUtil.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/utils/ContextUtil.kt
@@ -22,10 +22,12 @@ class ContextUtil {
 
             return  pref.getBoolean(AUTO_LOGIN, false)
         }
+//        토큰값을 TOKEN에 String값을 저장
         fun setToken( context: Context, token: String) {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             pref.edit().putString(TOKEN, token).apply()
         }
+//        토큰값 조회, 비어 있을 수 있음. "" 표시
         fun getToken( context: Context ) : String {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             return pref.getString(TOKEN, "")!!
