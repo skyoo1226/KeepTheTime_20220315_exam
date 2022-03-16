c40486280eaae3da046cb3875e15308aafca86a3
skyoo1226 <you1226@naver.com>
2022-03-16 22:25:59 +0900

    메인화면의 뷰페이저 어댑터 적용 - 아직 탭레이아웃 제목은 없음.

--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/MainActivity.kt
@@ -46,6 +46,8 @@ class MainActivity : BaseActivity() {
         mAdapter = MainViewPagerAdapter(supportFragmentManager)
         binding.mainViewPager.adapter = mAdapter
 
+        binding.mainTabLayout.setupWithViewPager(binding.mainViewPager)
+
     }
 
 }
