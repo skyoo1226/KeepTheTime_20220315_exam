9622f2b156d78db8e79d1409fa1a80799bf666c3
skyoo1226 <you1226@naver.com>
2022-03-16 23:48:38 +0900

    시작점 SplashActivity 초기 세팅

---
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.skyoo.keepthetime_20220315_exam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvens()
+        setValues()
+    }
+
+    override fun setupEvens() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
