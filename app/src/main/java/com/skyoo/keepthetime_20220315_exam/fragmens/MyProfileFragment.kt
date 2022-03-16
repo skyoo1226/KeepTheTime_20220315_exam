01af6e8edbdeab1b2bc0e5df2db25544333d7aee
skyoo1226 <you1226@naver.com>
2022-03-16 22:02:34 +0900

    내정보 프래그먼트(MyProfileFragment)를 BaseFragment 상속받아 완성.

---
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/fragmens/MyProfileFragment.kt
@@ -0,0 +1,33 @@
+package com.skyoo.keepthetime_20220315_exam.fragmens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.skyoo.keepthetime_20220315_exam.R
+
+class MyProfileFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_myprofile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
