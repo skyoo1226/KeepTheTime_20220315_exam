9024ee1bfb78d62b9f93d880c39ea6127134d328
skyoo1226 <you1226@naver.com>
2022-03-16 21:57:52 +0900

    약속목록 프래그먼트(AppointmentListFragment)를 BaseFragment 상속받아 완성.

--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/fragmens/AppointmentListFragment.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/fragmens/AppointmentListFragment.kt
@@ -1,4 +1,33 @@
 package com.skyoo.keepthetime_20220315_exam.fragmens
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.skyoo.keepthetime_20220315_exam.R
+
 class AppointmentListFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_appointment_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
 }
