6a603042dc4f549aa5f096eff048c79a90efcd6d
skyoo1226 <you1226@naver.com>
2022-03-20 07:31:01 +0900

    리싸이클러뷰의 어댑터 작업 중 - 3개의 필수 구현 함수 자동 오버라이딩 : fun onCreateViewHolder(), fun onBindViewHolder(),fun getItemCount()
    - 필요 자료 2개를 멤버변수로 저장(생성자 활용) : val mContext: Context, val mList: List<UserData>

--- app/src/main/java/com/skyoo/keepthetime_20220315_exam/adapters/MyFriendRecyclerAdapter.kt
+++ app/src/main/java/com/skyoo/keepthetime_20220315_exam/adapters/MyFriendRecyclerAdapter.kt
@@ -1,4 +1,29 @@
 package com.skyoo.keepthetime_20220315_exam.adapters
 
-class MyFriendRecyclerAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.skyoo.keepthetime_20220315_exam.datas.UserData
+
+class MyFriendRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<UserData>
+) : RecyclerView.Adapter<MyFriendRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
 }
